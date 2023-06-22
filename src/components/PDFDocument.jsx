ea5a054f861fbc162ecbca4e412aa9308cc69f9f
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2023-06-22 19:03:45 -0300

    Update PDFDocument.jsx

--- src/components/PDFDocument.jsx
+++ src/components/PDFDocument.jsx
@@ -1,13 +1,40 @@
-import { Document, Page, Text, View } from "@react-pdf/renderer";
+import { Document, Page, StyleSheet, Text, View } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    display: "flex",
+    margin: 20,
+    padding: 15,
+  },
+  checkbox: {
+    border: "1px solid black",
+    width: 18,
+    height: 18,
+    marginRight: 15,
+  },
+  row: {
+    marginBottom: 15,
+    border: "1px solid grey",
+    padding: 7,
+    borderRadius: 4,
+    maxWidth: 400,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  horario: { textTransform: "capitalize" },
+});
 
 export const PDFDocument = ({ hours }) => (
   <Document>
-    <Page>
-      <View>
+    <Page size="A4" style={styles.page}>
+      <View style={{}}>
         {hours.map((hour, index) => (
-          <Text key={index} style={{ fontSize: 20, fontWeight: 300 }}>
-            {`${hour.proximoHorario} - ${hour.medicacion}`}
-          </Text>
+          <View style={styles.row} key={index}>
+            <View style={styles.checkbox} />
+            <Text style={styles.horario}>
+              {`${hour.proximoHorario} - ${hour.medicacion}`}
+            </Text>
+          </View>
         ))}
       </View>
     </Page>
